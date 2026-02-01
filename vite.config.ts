cb6f1b4bf74c2b83890de27ee884ca7580804c4d
brandonlcosta <brandonlcosta@gmail.com>
2026-02-01 00:25:39 -0800

    2/1-2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   root: path.resolve(__dirname, "src/ui"),
   publicDir: path.resolve(__dirname, "public"),
